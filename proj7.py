f789cf06b48e4f8c1a4974d311e38ae16ab0549d
Michael Koundouros <michael.koundouros@outlook.com>
2020-09-14 13:18:03 +0100

    first commit

---
+++ proj7.py
@@ -0,0 +1,12 @@
+import sys
+
+msg = 'hello world!'
+
+
+print(msg)
+print(sys.version)
+print(sys.executable)
+
+
+print('ok')
+print('ok2')
---
+++ proj7b.py
@@ -0,0 +1,7 @@
+def greeting(name):
+    print(f'Hello {name}')
+
+
+myname = 'John'
+
+greeting(myname)
